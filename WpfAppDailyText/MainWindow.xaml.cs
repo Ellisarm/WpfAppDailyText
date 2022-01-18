4f9025444698182279c4e3c65b5860a15a6699b4
Ellisarm <armeanca_e2004@yahoo.com>
2022-01-18 23:16:44 +0200

    letsgoo

--- WpfAppDailyText/MainWindow.xaml.cs
+++ WpfAppDailyText/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Markup;
+using System.Diagnostics;
+using System.Windows.Navigation;
 
 namespace WpfAppDailyText
 {
@@ -17,21 +19,19 @@ namespace WpfAppDailyText
 
     public partial class MainWindow : INotifyPropertyChanged
     {
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public MainWindow()
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        {
 
-   
-            DataContext = this;
-           
+        {
+            InitializeComponent();
             checkDays();
             gettingData();
-
-            InitializeComponent();
         }
 
-
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
+        }
 
         private void gettingData()
         {
@@ -44,37 +44,63 @@ namespace WpfAppDailyText
                 HtmlDocument htmlDocument = new HtmlDocument();
                 htmlDocument.LoadHtml(html);
 
-                var ceva3 = htmlDocument.DocumentNode.CssSelect("header>h2").ToList();
-                Titlu = ceva3[1].InnerText;
+                /* var ceva3 = htmlDocument.DocumentNode.CssSelect("header>h2").ToList();
+                 Titlu = ceva3[1].InnerText;*/
+                var ceva3 = htmlDocument.DocumentNode.CssSelect("header>h2>").ToList();
+                string Titlu = ceva3[1].InnerText;
+                Titlu = "<TextBlock  xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" Grid.Row=\"0\"  FontSize=\"16\" FontWeight=\"Bold\">" + Titlu + "</TextBlock>";
+                grid.Children.Add(CreateTextBlock(Titlu));
+
+
+
+                /*var ceva = htmlDocument.DocumentNode.CssSelect(".themeScrp").ToList();
+                Verset = ceva[1].InnerText;*/
 
                 var ceva = htmlDocument.DocumentNode.CssSelect(".themeScrp").ToList();
-                Verset = ceva[1].InnerText;
+                string Verset = ceva[1].InnerHtml;
+                var vReplacement = Verset.Replace("em>", "Italic>");
+                var vReplacement2 = vReplacement.Replace("<a href", "<Hyperlink NavigateUri");
+                var vReplacement3 = vReplacement2.Replace("/ro", "https://wol.jw.org/ro");
+                var vReplacement4 = vReplacement3.Replace("class=\"b\"", " ");
+                var vReplacement5 = vReplacement4.Replace("</a>", "</Hyperlink>");
+                var vReplacement6 = vReplacement5.Replace("data-bid", "Tag");
+                vReplacement6 = "<TextBlock Grid.Column = \"0\" Grid.Row = \"1\" TextWrapping = \"Wrap\" FontSize = \"14\" Margin = \"0,0,0,10\" xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\">" + vReplacement6 + "</TextBlock>";
+                grid.Children.Add(CreateTextBlock(vReplacement6));
 
+
+
+                /*var ceva2 = htmlDocument.DocumentNode.CssSelect(".bodyTxt > div >div").ToList();
+                Comentariu = ceva2[1].InnerText;*/
                 var ceva2 = htmlDocument.DocumentNode.CssSelect(".bodyTxt > div >div").ToList();
-                Comentariu = ceva2[1].InnerText;
+                string Comentariu = ceva2[1].InnerHtml;
+                var replacement = Comentariu.Replace("em>", "Italic>");
+                var replacement2 = replacement.Replace("<a href", "<Hyperlink NavigateUri");
+                var replacement3 = replacement2.Replace("/ro", "https://wol.jw.org/ro");
+                var replacement4 = replacement3.Replace("class=\"b\"", " ");
+                var replacement5 = replacement4.Replace("</p>", "</TextBlock>");
+                var replacement6 = replacement5.Replace("data-pid", "Tag");
+                var replacement7 = replacement6.Replace("id=\"p", "Uid=\"");
+                var replacement8 = replacement7.Replace("</a>", "</Hyperlink>");
+                var replacement9 = replacement8.Replace("<p", "<TextBlock xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" Grid.Column=\"0\" Grid.Row=\"2\"   MaxWidth=\"400\" TextWrapping=\"Wrap\"  VerticalAlignment=\"Stretch\" HorizontalAlignment=\"Center\" FontSize=\"14\" ");
+                var replacement10 = replacement9.Replace("class=\"sb\"", " ");
+                var replacement11 = replacement10.Replace("data-bid", "Tag");
+                grid.Children.Add(CreateTextBlock(replacement11));
+
+                grid.AddHandler(Hyperlink.RequestNavigateEvent, new RequestNavigateEventHandler(Hyperlink_RequestNavigate));
             }
-            else
-            {
-                Titlu = "page not found";
-                Comentariu = " ";
-                Verset = " ";
-            }
+           
 
 
         }
 
         public TextBlock CreateTextBlock(string inlines)
         {
-            var xaml = "<TextBlock xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\">"
-                + inlines + "</TextBlock>";
-            return XamlReader.Parse(xaml) as TextBlock;
+            return XamlReader.Parse(inlines) as TextBlock;
         }
 
 
         private string url;
-        private string titlu;
-        private string verset;
-        private string comentariu;
+        
         private int currentYear = DateTime.Now.Year;
         private int currentMonth = DateTime.Now.Month;
         private int currentDay = DateTime.Now.Day;
@@ -144,42 +170,7 @@ namespace WpfAppDailyText
                 }
             }
         }
-        public string Titlu
-        {
-            get { return titlu; }
-            set
-            {
-                if (titlu != value)
-                {
-                    titlu = value;
-                    OnPropertyChanged("Titlu");
-                }
-            }
-        }
-        public string Verset
-        {
-            get { return verset; }
-            set
-            {
-                if (verset != value)
-                {
-                    verset = value;
-                    OnPropertyChanged("Verset");
-                }
-            }
-        }
-        public string Comentariu
-        {
-            get { return comentariu; }
-            set
-            {
-                if (comentariu != value)
-                {
-                    comentariu = value;
-                    OnPropertyChanged("Comentariu");
-                }
-            }
-        }
+        
         public int CurrentYear
         {
             get { return currentYear; }
@@ -238,6 +229,7 @@ namespace WpfAppDailyText
 
         private void NextDay_Click(object sender, RoutedEventArgs e)
         {
+            grid.Children.RemoveRange(2, 3);
             if (thirty)
             {
                 if (currentDay == 30)
@@ -311,6 +303,7 @@ namespace WpfAppDailyText
 
         private void PreviousDay_Click(object sender, RoutedEventArgs e)
         {
+            grid.Children.RemoveRange(2, 3);
             if (currentDay == 1)
             {
                 if (thirty)
